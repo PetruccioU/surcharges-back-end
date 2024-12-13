c4a37c84189b46b8bba903b096fc646520c8607c
Petr <petr.utkin2022@gmail.com>
2024-12-13 14:14:58 +1300

    PlaceEntity.cs added

---
+++ src/Domain/Entities/PlaceEntity.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+public class PlaceEntity: BaseEntity
+{
+    public string? PlaceName { get; set; }
+    
+    public string? PlaceDescription { get; set; }
+    
+    public string? PlaceImage { get; set; }
+    
+    public required string PlaceApiId { get; set; }
+    
+    public PlaceEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    } 
+}
