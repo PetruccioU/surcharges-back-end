b40c008e33039f7cd92063f6018a27eba20a3217
Petr <petr.utkin2022@gmail.com>
2024-12-13 13:45:09 +1300

    BaseEntity.cstity added, SurchargePlaceEntity.cs added

---
+++ src/Domain/Entities/SurchargePlaceEntity.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+namespace Domain.Entities;
+
+public class SurchargePlaceEntity: BaseEntity
+{
+    public double SurchargePercentage { get; set; }
+    
+    public string? SurchargeDescription { get; set; }
+    
+    public string? SurchargePaymentMethod { get; set; }
+    
+    public string? SurchargeImage { get; set; }
+    
+    public required Guid PlaceId { get; set; }
+    
+    public SurchargePlaceEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
