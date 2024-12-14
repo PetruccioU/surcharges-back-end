67d194c56371e69cfbca0a2c4ec346594bc07950
Petr <petr.utkin2022@gmail.com>
2024-12-15 09:18:32 +1300

    dbcontext created

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<SurchargeEntity> SurchargeEntity { get; }
+    
+    DbSet<SurchargeListEntity> SurchargeListEntity { get; }
+    
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ src/Infrastructure/Data/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+// using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.DbContext;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<SurchargeEntity> SurchargeEntity => Set<SurchargeEntity>();
+
+    public DbSet<SurchargeListEntity> SurchargeListEntity => Set<SurchargeListEntity>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
