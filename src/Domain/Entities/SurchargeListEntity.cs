db26b7acb61490af149aafbf33bed988f8f744d2
Petr <petr.utkin2022@gmail.com>
2024-12-14 15:00:23 +1300

    SurchargeListEntity.cs added

---
+++ src/Domain/Entities/SurchargeListEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class SurchargeListEntity: BaseEntity
+{
+    public string? Title { get; set; }
+
+    public IList<SurchargeEntity> SurchargesList { get; private set; } = new List<SurchargeEntity>();
+}
